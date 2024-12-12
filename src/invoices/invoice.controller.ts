e160a15c7fb3724643288f249ce1aceb8bfe3416
Igor Silva <igor-gab@hotmail.com>
2024-12-12 12:38:13 -0300

    add: healtcheck function

--- src/invoices/invoice.controller.ts
+++ src/invoices/invoice.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Headers } from '@nestjs/common';
+import { Body, Controller, Post, Headers, Get } from '@nestjs/common';
 import { InvoiceService } from './invoice.service';
 import { Cron } from '@nestjs/schedule';
 import { Invoice, Transfer } from 'starkbank';
@@ -6,6 +6,11 @@ import { Invoice, Transfer } from 'starkbank';
 @Controller()
 export class InvoiceController {
   constructor(private readonly invoiceService: InvoiceService) {}
+
+  @Get('healtcheck')
+  functionsHealthcheck() {
+    return 'The app is running normally';
+  }
 
   @Post('/invoice/process')
   processTransfer(
